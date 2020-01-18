d15e28f6b5bdf39adcbfa008db67762735e89380
nitrojs <n1trojscoding@mail.ru>
2020-01-18 14:06:41 +0300

    Add isContaints and isContaintsCycle

---
+++ isContaints.js
@@ -0,0 +1,10 @@
+/**
+ * Creator: https://vk.com/id275539712
+ * Chat: Shaotop BOT
+ * VK post: https://vk.com/codersmemes?w=wall-188566339_197
+ */
+
+function isContaints(arr, index)
+{
+  return arr[index] ? true : false;
+}
---
+++ isContaintsCycle.js
@@ -0,0 +1,19 @@
+/**
+ * Creator: https://vk.com/id275539712
+ * Chat: Shaotop BOT
+ * VK post: https://vk.com/codersmemes?w=wall-188566339_197
+ */
+
+var bool
+for(let i = 0; i <= номер; i++)
+{
+  if(i == номер)
+  {
+    if(object[i])
+    {
+      bool = true
+    } else {
+      bool = false
+    }
+  }
+}
